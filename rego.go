554d9467aa778bbcf24926817d8513cff0c66d18
Dmitri Shuralyov <shurcooL@gmail.com>
2016-02-22 11:21:18 -0800

    Fix /vendor/ support for Go 1.6.
    Provide correct working directory to second instance of build.Import. The first one already has wd correctly provided.

--- rego.go
+++ rego.go
@@ -80,7 +80,7 @@ func main() {
 				if imp == "C" {
 					continue
 				}
-				impPkg, err := build.Import(imp, "", 0)
+				impPkg, err := build.Import(imp, wd, 0)
 				if err != nil {
 					log.Fatal(err)
 				}
